83220c5d6b4d04d75ae7821d87b1434eb2bcebac
MOBIN MOSTAFA <mobinmostafa920@gmail.com>
2024-01-17 23:58:47 +0600

    production component

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react"
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from "./components"
+
 
 function App() {
+   const [loading, setLoading] = useState(true)
+   const dispatch = useDispatch()  
    
-  return (
+   useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+          if(userData){
+            dispatch(login({userData}))
+          }else{
+            dispatch(logout())
+          }
+      })
+      .finally(() => setLoading(false))
+   }, [])
+
+return !loading ? (
   <>
-    <h1>blog practice</h1>
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+       <div className="w-full block">
+        <Header/>
+        <Footer/>
+       </div>
+    </div>
   </>
-  )
+) : null
+
 }
 
 export default App
---
+++ blog/src/components/Button.jsx
@@ -0,0 +1,15 @@
+
+
+const Button = ({
+    children,
+    type="button",
+    bgColor= "bg-blue-600",
+    textColor= "text-white",
+    ...props
+}) => {
+  return (
+    <button className={`px-4 py-2 rounded-lg ${textColor} ${bgColor} ${className}`} {...props}>{children}</button>
+  )
+}
+
+export default Button
